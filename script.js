08e6946dc8101103f78e9de64ceafd36e64b624f
Nina B <ngbuscemi@gmail.com>
2023-12-10 22:29:53 -0500

    Added password requirement prompts

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+
+  // Password Requirement prompts generated
+  var symbols = confirm("Your password requires atleast one symbol. Click OK to continue.");
+  var numbers = confirm("Your password requires atleast one number. Click OK to continue.");
+  var UpperCase = confirm("Your password requires atleast one letter. Click OK to continue.");
+  var LowerCase = confirm("Your password requires atleast one lowercase letter. Click OK to continue.");
+  var keyLength = confirm("Your password requires atleast one symbol. Click OK to continue.");
+}
+  
 
 // Write password to the #password input
 function writePassword() {
